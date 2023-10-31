898d1298f8cf03f17b081b1e92cdc74c620ccdde
andrea <amndrea30@gmail.com>
2023-10-31 15:43:52 +0100

    prenotazione

--- Prenotazioni/urls.py
+++ Prenotazioni/urls.py
@@ -6,9 +6,12 @@ app_name = "Prenotazioni"
 urlpatterns = [
 
     # URL al quale mostro il calendario a partire dal mese corrente
-    path("mostra_calendario/",mostra_calendario,name="calendario"),
+    path("mostra_calendario/<int:user_pk>/",mostra_calendario,name="calendario"),
 
     # URL al quale visualizzo gli orari prenotabili di un dato giorno
     path("mostra_giorno/<int:year>/<int:month>/<int:day>/", mostra_giorno, name="mostra_giorno"),
+
+    # URL al quale effettuo una prenotazione per un orario
+    path("prenota/<str:orario>/<int:giorno_pk>/<int:utente_pk>/", effettua_prenotazione, name="prenota"),
 
 ]
--- Prenotazioni/views.py
+++ Prenotazioni/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 import datetime
+
+from django.urls import  reverse
+
+from Users.models import MyUser
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
 
 # View per mostrare una pagina HTML contenente un calendario
-def mostra_calendario(request):
+def mostra_calendario(request, user_pk):
     parametri = request.GET
     if 'year' in parametri and 'month' in parametri:
         anno = int(parametri['year'])
@@ -16,30 +21,62 @@ def mostra_calendario(request):
 
 
 
-# View per mostrare i giorni e permettere il prenotamento di un orario
+# View per mostrare i giorni e permettere il
+# prenotamento di un orario
 def mostra_giorno(request, year, month, day):
     data = datetime.date(year,month,day)
     ctx = {}
     # Domenica la palestra è chiusa tutto il giorno
     domenica = 1 if data.weekday() == 6 else 0
+    prenotazione = None
 
     if not domenica:
         try:
-            giorno_i = Giorno.objects.get(anno=year,mese=month,giorno=day)
+            giorno_i = Giorno.objects.get(anno=year, mese=month, giorno=day)
         except ObjectDoesNotExist:
             giorno_i, creato = Giorno.objects.get_or_create(anno=year, mese=month, giorno=day)
         ctx["giorno"] = giorno_i
+        prenotazione = datetime.date(giorno_i.anno, giorno_i.mese, giorno_i.giorno)
 
-    ctx["doemnica"] = domenica
+    ctx["domenica"] = domenica
     oggi = datetime.date.today()
 
-    if giorno_i.anno == oggi.year and giorno_i.mese == oggi.month:
-        if giorno_i.giorno < oggi.day:
-            pass
-    # Controllo che il giorno della prenotazione sia possibile
-    if giorno_i.anno < oggi.year:
-        ctx['errore'] = True
+    if prenotazione is not None and prenotazione < oggi and not domenica:
+        return render(request, "Prenotazioni/errore.html")
     else:
+        orari = ["8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00"]
+        ctx['orari'] = orari
+        return render(request, "Prenotazioni/giorno.html",ctx)
 
-        print("si")
-    return render(request,'Prenotazioni/giorno.html',ctx)
+def effettua_prenotazione(request, orario, giorno_pk, utente_pk):
+
+    # Conversione da orario in formato stringa a intero
+    i = orario.find(":")
+    orario = orario[0:i]
+    orario = int(orario)
+
+
+    # Controllo che l'utente abbia l'abbonamento valido, se non è valido mando alla sezione personale
+    my_user = MyUser.objects.get(pk=utente_pk)
+    oggi = datetime.date.today()
+
+    if my_user.fine_abbonamento < oggi:
+        return HttpResponseRedirect(reverse('Users:situazione', args=[my_user.pk]))
+
+
+    # Controllo che la prenotazione dell'utente non ci sia gia in quel giorno e in quell'ora
+    # Se non c'è la inserisco e la
+    giorno = Giorno.objects.get(pk=giorno_pk)
+    try:
+        prenotazione = Prenotazione.objects.get(giorno=giorno, user=my_user, ora=orario)
+        print("prenotazione gia effettuata merda")
+
+    except ObjectDoesNotExist:
+        prenotazione = Prenotazione()
+        prenotazione.giorno = giorno
+        prenotazione.user = my_user
+        prenotazione.ora = orario
+        prenotazione.save()
+        print("prenotazione creata")
+
+    return render(request, "Prenotazioni/prenota.html")
